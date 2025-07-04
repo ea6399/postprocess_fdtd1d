47db69e1add8e8d5e3d18252df6ef4dd450379f7
ALI Emin <eminali2430@gmail.com>
2025-07-04 16:27:28 +0200

    update

--- phase.m
+++ phase.m
@@ -54,7 +54,7 @@ S = CFL;
 
 
 %% Sélection du point spatial
-x_j0 = int32(Nx/2); % Indice spatial
+x_j0 = 250; % Indice spatial
 e_fd = E_fd(x_j0, :); % Signal temporel FD
 e_cn = E_cn(x_j0, :); % Signal temporel CN
 
@@ -68,38 +68,53 @@ Ecn_fft = fft(e_cn);
 
 n_ech = length(e_fd);
 fprintf("\n Echantillon temporels : %d \n", size(e_fd))
-T_max = n_ech * snapshot * dt;
+T_max =  n_ech * snapshot * dt ;
 fs = 1 / (snapshot * dt); % Fréquence d'échantillonnage [Hz]
-fprintf('\n Fréquence d''échantillonnage fs: %.3e Hz', fs);
+fprintf('\n Fréquence d''échantillonnage fs: %.3e Hz \n', fs);
 df = 1 / T_max;           % Résolution fréquentielle
 f_axis = ( -Nt/2 : Nt/2 - 1 )*df;
-% Conserve les fréquence positive uniquement
-f_pos = f_axis >= 0;            % Masque des fréquence positive
-f_axis_pos = f_axis(f_pos);
+f_pos = f_axis >= 0;            % Masque booléen des fréquences positives
+f_ax_pos = f_axis(f_pos);
 
 z_fd = Efd_fft;
 z_cn = Ecn_fft;
 
-z_fd_pos = z_fd(1 : length(f_axis_pos));
-z_cn_pos = z_cn(1 : length(f_axis_pos));
+z_fd_pos = z_fd(1 : length(f_ax_pos));
+z_cn_pos = z_cn(1 : length(f_ax_pos));
 
+
+%% Phase théorique
+x_obs = x_j0 * dx;              % Position du point d'observation
+omega_pos = 2 * pi * f_ax_pos;  % Fréquence angulairs
+
+k_theo = omega_pos / c;
+phase_theo =  k_theo * x_obs;
+% 
+% % Relations de dispersion numériques théoriques
+% k_fd_theo = (2/dx) * asin((1/S) * sin(omega_pos * dt/2));
+% k_cn_theo = (2/dx) * atan((1/S) * sin(omega_pos * dt/2));
+% 
+% % Phases théoriques correspondantes
+% phase_fd_theo = k_fd_theo * x_obs;
+% phase_cn_theo = k_cn_theo * x_obs;
 
 
 
 
 %% Tracé du spectre
 figure('Position', [100 100 1200 800], 'Color','white');
-subplot(1,2,1)
-plot(f_axis_pos,abs(z_fd_pos),'DisplayName','FDTD'); hold on
-plot(f_axis_pos,abs(z_cn_pos),'--','DisplayName','CNFDTD')
+subplot(1,2,1);
+plot(f_ax_pos,abs(z_fd_pos),'DisplayName','FDTD', LineWidth=1); hold on
+plot(f_ax_pos,abs(z_cn_pos),'--','DisplayName','CNFDTD', LineWidth=1)
 xlabel('Fréquence (Hz)');
 ylabel('Amplitude Champ E');
 legend('show');
 grid on;
 
 subplot(1,2,2);
-plot(f_axis_pos,unwrap(angle(z_fd_pos)),'DisplayName','FDTD'); hold on
-plot(f_axis_pos,unwrap(angle(z_cn_pos)),'--','DisplayName','CNFDTD');
+plot(f_ax_pos,unwrap(angle(z_fd_pos),[],2),'DisplayName','FDTD', LineWidth=1); hold on
+plot(f_ax_pos,unwrap(angle(z_cn_pos),[],2),'--','DisplayName','CNFDTD',LineWidth=1);
+plot(f_ax_pos, phase_theo, 'k-', 'DisplayName', 'Physique',LineWidth=1);
 title('Phase');
 xlabel('Frequency [Hz]');
 ylabel('Phase / \pi');
