e9b1e7ef777a2ead48ff01a47287f4f75bdedbcf
ALI Emin <eminali2430@gmail.com>
2025-07-07 11:40:21 +0200

    update

--- phase.m
+++ phase.m
@@ -72,36 +72,47 @@ T_max =  n_ech * snapshot * dt ;
 fs = 1 / (snapshot * dt); % Fréquence d'échantillonnage [Hz]
 fprintf('\n Fréquence d''échantillonnage fs: %.3e Hz \n', fs);
 df = 1 / T_max;           % Résolution fréquentielle
-f_axis = ( -Nt/2 : Nt/2 - 1 )*df;
-f_pos = f_axis >= 0;            % Masque booléen des fréquences positives
-f_ax_pos = f_axis(f_pos);
+f_axis = ( 0 : n_ech - 1 )*df;
+f_semi = f_axis <= fs/4;            % Masque booléen des fréquences positives
+f_ax_pos = f_axis(f_semi);
 
 z_fd = Efd_fft;
 z_cn = Ecn_fft;
 
-z_fd_pos = z_fd(1 : length(f_ax_pos));
-z_cn_pos = z_cn(1 : length(f_ax_pos));
 
+z_fd_pos = z_fd(f_semi);
+z_cn_pos = z_cn(f_semi);
+
+omega_pos = 2 * pi * f_ax_pos;  % Fréquence angulairs
+
+%% Phase
+x_obs = x_j0 * dx;              % Position du point d'observation
+k_theo = omega_pos / c;                                     % Onde physique
+
+phase_theo = k_theo * x_obs;
 phase_fd = unwrap(angle(z_fd_pos));
 phase_cn = unwrap(angle(z_cn_pos));
 
-
-%% Phase théorique
-x_obs = x_j0 * dx;              % Position du point d'observation
-omega_pos = 2 * pi * f_ax_pos;  % Fréquence angulairs
-
-k_theo = omega_pos / c;
-k_fd_measured =  phase_fd ./x_obs;
-k_cn_measured =  phase_cn ./ x_obs;
-
-% % Relations de dispersion numériques théoriques
-k_fd_theo = (2/dx) * asin((1/S) * sin(omega_pos * dt/2));
+%% Calcul des nombres d'onde
+k_fd_measured =  phase_fd ./x_obs;                          % Dispersion numerique FDTD
+k_cn_measured =  phase_cn ./ x_obs;                         % Dispersion numérique CN
+k_fd_theo = (2/dx) * asin((1/S) * sin(omega_pos * dt/2));   % Relations de dispersion numériques théoriques
 k_cn_theo = (2/dx) * atan((1/S) * sin(omega_pos * dt/2));
 
 
+%% Calcul des vitesses relatives
+vp = (omega_pos ./ k_theo) / c;  % Vitesse de phase théorique
+vp_fd = (omega_pos ./ k_fd_theo) / c;  % Vitesse de phase FDTD
+vp_cn = (omega_pos ./ k_cn_theo) / c;  % Vitesse de phase CN
+vp_fd_measured = (omega_pos ./ k_fd_measured) / c;  % Vitesse de phase mesurée FDTD
+vp_cn_measured = (omega_pos ./ k_cn_measured) / c;  % Vitesse de phase mesurée CN
+
+
 %% Tracé du spectre
+
+% Amplitude de l'onde
 figure('Position', [100 100 1200 800], 'Color','white');
-subplot(1,3,1);
+subplot(2,2,1);
 plot(f_ax_pos,abs(z_fd_pos),'DisplayName','FDTD', LineWidth=1); hold on
 plot(f_ax_pos,abs(z_cn_pos),'--','DisplayName','CNFDTD', LineWidth=1)
 xlabel('Fréquence (Hz)');
@@ -109,7 +120,8 @@ ylabel('Amplitude Champ E');
 legend('show');
 grid on;
 
-subplot(1,3,2);
+% Etude de phase
+subplot(2,2,2);
 plot(f_ax_pos,unwrap(angle(z_fd_pos),[],2),'DisplayName','FDTD', LineWidth=1); hold on
 plot(f_ax_pos,unwrap(angle(z_cn_pos),[],2),'--','DisplayName','CNFDTD',LineWidth=1);
 plot(f_ax_pos, phase_theo, 'k-', 'DisplayName', 'Physique',LineWidth=1);
@@ -119,11 +131,29 @@ ylabel('Phase / \pi');
 legend('Show')
 grid on;
 
-subplot(1,3,3);
-plot(f_ax_pos, k_fd_theo,'DisplayName','FD theo'); hold on
-plot(f_ax_pos, k_cn_theo,'DisplayName','CN theo');
-plot(f_ax_pos, k_fd_measured, 'DisplayName','FD exp');
-plot(f_ax_pos, k_cn_measured, 'DisplayName','CN exp');
-plot(f_ax_pos, k_theo, 'DisplayName','Physique');
+% Dispersions numeriques
+subplot(2,2,3);
+plot(omega_pos, k_fd_theo,'DisplayName','FD theo'); hold on
+plot(omega_pos, k_cn_theo,'DisplayName','CN theo');
+plot(omega_pos, k_fd_measured, 'DisplayName','FD exp');
+plot(omega_pos, k_cn_measured, 'DisplayName','CN exp');
+plot(omega_pos, k_theo, 'k-', 'DisplayName','Physique');
+legend('show');
+xlabel('Fréquences Angulaire [rad/s]')
+ylabel('Nombre d''onde k [rad/m]')
+title('Relation de dispersion')
+grid on;
+
+% Vitesses relatives
+subplot(2,2,4)
+plot(omega_pos, vp, 'k-', 'DisplayName','Physique'); hold on
+plot(omega_pos, vp_fd, 'DisplayName','FD théorique');
+plot(omega_pos, vp_cn, 'DisplayName','CN théorique');
+plot(omega_pos, vp_fd_measured, 'DisplayName','FD mesuré');
+plot(omega_pos, vp_cn_measured, 'DisplayName','CN mesuré');
+xlabel('Fréquence Angulaire [rad/s]');
+ylabel('Vitesse de phase relative');
 legend('show');
 grid on;
+
+
