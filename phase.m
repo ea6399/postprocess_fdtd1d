0b8cd5f5a752bb118e0d46c548fc4d04b2f2e69d
ALI Emin <eminali2430@gmail.com>
2025-07-10 14:40:25 +0200

    Tout le signal est analyser

--- phase.m
+++ phase.m
@@ -53,35 +53,60 @@ CFL = 0.5;
 S = CFL;
 
 
+
 %% Sélection du point spatial
+% Point à la source
+x_src = 1;
 x_j0 = 250; % Indice spatial
-e_fd = E_fd(x_j0, :); % Signal temporel FD
-e_cn = E_cn(x_j0, :); % Signal temporel CN
+e_fd_src = E_fd(x_src,:);           % Signal à la source
+e_cn_src = E_cn(x_src,:);
+e_fd = E_fd(x_j0, :);               % Signal temporel FD
+e_cn = E_cn(x_j0, :);               % Signal temporel CN
 
 fprintf('\n Taille e_fd: %d\n', length(e_fd));
 fprintf('\n Taille e_cn: %d\n', length(e_cn));
 
 %% Analyse spectrale par FFT
 % Calcul des FFT
-Efd_fft = fft(e_fd);
+Efd_src = fft(e_fd_src);
+Ecn_src = fft(e_cn_src);
+Efd_fft = fft(e_fd);  % /fft(e_fd_src)
 Ecn_fft = fft(e_cn);
 
+% On retire la phase à l'origine
+z_fd = Efd_fft ./ Efd_src;
+z_cn = Ecn_fft ./ Ecn_src;
+
+% Parametrage des axes
 n_ech = length(e_fd);
 fprintf("\n Echantillon temporels : %d \n", size(e_fd))
 T_max =  n_ech * snapshot * dt ;
 fs = 1 / (snapshot * dt); % Fréquence d'échantillonnage [Hz]
 fprintf('\n Fréquence d''échantillonnage fs: %.3e Hz \n', fs);
 df = 1 / T_max;           % Résolution fréquentielle
+
+
 f_axis = ( 0 : n_ech - 1 )*df;
 f_semi = f_axis <= fs/4;            % Masque booléen des fréquences positives
 f_ax_pos = f_axis(f_semi);
-
-z_fd = Efd_fft;
-z_cn = Ecn_fft;
-
-
 z_fd_pos = z_fd(f_semi);
 z_cn_pos = z_cn(f_semi);
+Efd_pos = Efd_fft(f_semi);
+Ecn_pos = Ecn_fft(f_semi);
+
+% Détermine la plage d'énergie du signal
+% threshold = 0.5; % Seuil d'amplitude relative
+% amplitude_max = max(abs(Efd_fft));
+% mask_ampli =  abs(Efd_fft) / amplitude_max >= threshold ;     % indices des fréquences significatives
+% index_f_max = f_semi & mask_ampli;
+% f_plage = f_axis(index_f_max);  % Plage de fréquences significatives
+% f_ax_pos = f_axis(index_f_max);  % Fréquences positives significatives
+% z_fd_pos = z_fd(index_f_max);
+% z_cn_pos = z_cn(index_f_max);
+% Efd_pos = Efd_fft(index_f_max);
+% Ecn_pos = Ecn_fft(index_f_max);
+
+
 
 omega_pos = 2 * pi * f_ax_pos;  % Fréquence angulairs
 
@@ -90,13 +115,22 @@ x_obs = x_j0 * dx;              % Position du point d'observation
 k_theo = omega_pos / c;                                     % Onde physique
 
 phase_theo = k_theo * x_obs;
-phase_fd = unwrap(angle(z_fd_pos));
+phase_fd = unwrap(angle(z_fd_pos)); 
 phase_cn = unwrap(angle(z_cn_pos));
+% Ramener l'erreur de  phase par lamda :
+%  ErrPhase = (Ph_fd-Ph_theor) ./ (x*f/c)
+ErrPhase_fd = abs(phase_fd - phase_theo) ./ (x_obs .* f_ax_pos / c);
+ErrPhase_cn = abs(phase_cn - phase_theo) ./ (x_obs .* f_ax_pos / c);
 
 %% Calcul des nombres d'onde
 k_fd_measured =  phase_fd ./x_obs;                          % Dispersion numerique FDTD
 k_cn_measured =  phase_cn ./ x_obs;                         % Dispersion numérique CN
-k_fd_theo = (2/dx) * asin((1/S) * sin(omega_pos * dt/2));   % Relations de dispersion numériques théoriques
+ang_freq_fd = (2 / dt) * asin(S * sin(k_fd_measured * dx / 2)); 
+ang_freq_cn = (2 / dt) * atan(S * sin(k_cn_measured * dx / 2)); 
+theo_ang_freq_fd = (2 / dt) * asin(S * sin(k_theo * dx / 2)); 
+theo_ang_freq_cn = (2 / dt) * atan(S * sin(k_theo * dx / 2)); 
+
+k_fd_theo = (2/dx) * asin((1/S) * sin(omega_pos * dt/2));
 k_cn_theo = (2/dx) * atan((1/S) * sin(omega_pos * dt/2));
 
 
@@ -113,45 +147,45 @@ vp_cn_measured = (omega_pos ./ k_cn_measured) / c;  % Vitesse de phase mesurée 
 % Amplitude de l'onde
 figure('Position', [100 100 1200 800], 'Color','white');
 subplot(2,2,1);
-plot(f_ax_pos,abs(z_fd_pos),'DisplayName','FDTD', LineWidth=1); hold on
-plot(f_ax_pos,abs(z_cn_pos),'--','DisplayName','CNFDTD', LineWidth=1)
+% plot(f_ax_pos,abs(z_fd_pos),'DisplayName','FDTD', LineWidth=1); hold on
+% plot(f_ax_pos,abs(z_cn_pos),'--','DisplayName','CNFDTD', LineWidth=1)
+plot(f_ax_pos,abs(Efd_pos),'DisplayName','FDTD', LineWidth=1); hold on
+plot(f_ax_pos,abs(Efd_pos),'--','DisplayName','CNFDTD', LineWidth=1)
 xlabel('Fréquence (Hz)');
-ylabel('Amplitude Champ E');
+ylabel('Amplitude relative du champ E');
 legend('show');
 grid on;
 
 % Etude de phase
 subplot(2,2,2);
-plot(f_ax_pos,unwrap(angle(z_fd_pos),[],2),'DisplayName','FDTD', LineWidth=1); hold on
-plot(f_ax_pos,unwrap(angle(z_cn_pos),[],2),'--','DisplayName','CNFDTD',LineWidth=1);
-plot(f_ax_pos, phase_theo, 'k-', 'DisplayName', 'Physique',LineWidth=1);
-title('Phase');
+plot(f_ax_pos,ErrPhase_fd,'DisplayName','FDTD', LineWidth=1); hold on
+plot(f_ax_pos,ErrPhase_cn,'--','DisplayName','CNFDTD',LineWidth=1);
 xlabel('Frequency [Hz]');
-ylabel('Phase / \pi');
+ylabel('Erreur de phase relative');
 legend('Show')
 grid on;
 
 % Dispersions numeriques
 subplot(2,2,3);
-plot(omega_pos, k_fd_theo,'DisplayName','FD theo'); hold on
-plot(omega_pos, k_cn_theo,'DisplayName','CN theo');
-plot(omega_pos, k_fd_measured, 'DisplayName','FD exp');
-plot(omega_pos, k_cn_measured, 'DisplayName','CN exp');
-plot(omega_pos, k_theo, 'k-', 'DisplayName','Physique');
+plot(k_theo, theo_ang_freq_fd ./ c,'r--','DisplayName','FD theo','LineWidth',1); hold on
+plot(k_theo, theo_ang_freq_cn ./ c,'b--','DisplayName','CN theo','LineWidth',1);
+plot(k_theo, ang_freq_fd ./ c,'rs', 'DisplayName','FD exp','MarkerIndices', 1:10:length(k_theo));
+plot(k_theo, ang_freq_cn ./ c,'bo', 'DisplayName','CN exp','MarkerIndices', 1:10:length(k_theo));
+plot(k_theo, k_theo , 'k-', 'DisplayName','Physique', 'LineWidth', 1);
 legend('show');
-xlabel('Fréquences Angulaire [rad/s]')
-ylabel('Nombre d''onde k [rad/m]')
-title('Relation de dispersion')
+xlabel('k \Delta x [rad/m]')
+ylabel('\omega [rad/s]')
+title('Relation de dispersion numérique')
 grid on;
 
 % Vitesses relatives
 subplot(2,2,4)
-plot(omega_pos, vp, 'k-', 'DisplayName','Physique'); hold on
-plot(omega_pos, vp_fd, 'DisplayName','FD théorique');
-plot(omega_pos, vp_cn, 'DisplayName','CN théorique');
-plot(omega_pos, vp_fd_measured, 'DisplayName','FD mesuré');
-plot(omega_pos, vp_cn_measured, 'DisplayName','CN mesuré');
-xlabel('Fréquence Angulaire [rad/s]');
+idf = round(0.5 * length(omega_pos)); % 20% de la plage de fréquences
+plot(omega_pos(idf), vp(idf), 'ko', 'DisplayName','Physique', 'MarkerSize', 10); hold on
+plot(omega_pos(idf), vp_fd(idf), '>', 'DisplayName','FD théorique', 'MarkerSize', 10);
+plot(omega_pos(idf), vp_cn(idf), '+','DisplayName','CN théorique', 'MarkerSize', 10);
+plot(omega_pos(idf), vp_fd_measured(idf),'s' , 'DisplayName','FD mesuré', 'MarkerSize', 10);
+plot(omega_pos(idf), vp_cn_measured(idf), 'v', 'DisplayName','CN mesuré', 'MarkerSize', 10);
 ylabel('Vitesse de phase relative');
 legend('show');
 grid on;
